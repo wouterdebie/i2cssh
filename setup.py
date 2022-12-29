a6837610d52e6ba035e8c61e22578be92fbbe0c3
Wouter de Bie <wouter@evenflow.nl>
2022-12-29 14:28:21 -0500

    0.0.11: Fix version include

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 from setuptools import setup
-from version import version
+from .version import version
 
 
 setup(
--- version.py
+++ version.py
@@ -1,2 +1,2 @@
 def version():
-    return '0.0.10'
+    return '0.0.11'
